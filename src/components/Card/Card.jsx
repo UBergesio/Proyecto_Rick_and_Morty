965017da496ad0cf6efcdb52f2b88857eb668e5a
UBergesio <ulibergesio@gmail.com>
2023-06-08 23:49:38 -0300

    ccs

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -9,12 +9,15 @@ export default function Card(props) {
       >
         X
       </button>
+      <hr className={style.hr} />
+      <img className={style.image} src={props.image} alt="" />
       <h2 className={style.nombres}>{props.name}</h2>
+      <hr className={style.hr} />
       <h2 className={style.description}>{props.status}</h2>
       <h2 className={style.description}>{props.species}</h2>
       <h2 className={style.description}>{props.gender}</h2>
       <h2 className={style.descriptionOrigin}>{props.origin}</h2>
-      <img className={style.image} src={props.image} alt="" />
+      <hr className={style.hr} />
     </div>
   );
 }
